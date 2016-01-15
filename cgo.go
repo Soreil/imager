5208a28ed94d14a34e2243289d34cadc835a2e32
Sjon <sjon@x61.eu>
2016-01-15 21:21:15 +0100

    slowly learning more about how to do things with these libraries, now I need to refresh on how *uint_8t relates to pointers, they are bytes I guess

--- cgo.go
+++ cgo.go
@@ -1,12 +1,14 @@
 package imager
 
-// #cgo pkg-config: libavcodec libavutil
+// #cgo pkg-config: libavcodec libavutil libavformat
 //
 // #include "libavcodec/avcodec.h"
 // #include "libavutil/frame.h"
+// #include "libavformat/avformat.h"
 import "C"
 import (
-	"io/ioutil"
+	"errors"
+	"fmt"
 	"unsafe"
 )
 
@@ -14,6 +16,7 @@ func init() {
 	//Register all the codecs, parsers and bitstream filters which were enabled at configuration time.
 	//If you do not call this function you can select exactly which formats you want to support, by using the individual registration functions.
 	C.avcodec_register_all()
+	C.av_register_all()
 }
 
 type Decoder struct {
@@ -25,13 +28,13 @@ type Decoder struct {
 func NewVP8Decoder() *Decoder {
 	var dec Decoder
 	dec.codec = C.avcodec_find_decoder(C.AV_CODEC_ID_VP8)
-	//if dec.codec == nil {
-	//	panic(errors.New("Can't find codec"))
-	//}
+	if dec.codec == nil {
+		panic(errors.New("Can't find codec"))
+	}
 	dec.codecCtx = C.avcodec_alloc_context3(dec.codec)
-	//if dec.codecCtx == nil {
-	//	panic(errors.New("Can't create context"))
-	//}
+	if dec.codecCtx == nil {
+		panic(errors.New("Can't create context"))
+	}
 	dec.codecCtx.opaque = unsafe.Pointer(&dec)
 	C.avcodec_open2(dec.codecCtx, dec.codec, nil)
 	return &dec
@@ -40,29 +43,41 @@ func NewVP8Decoder() *Decoder {
 func (d *Decoder) DecodeVP8(fileName string) error {
 	var packet *C.AVPacket
 	var frame *C.AVFrame
+	var formatCtx *C.AVFormatContext = nil
+	var pict *C.AVPicture = nil
 	var got C.int
+
+	cs := C.CString(fileName)
+	defer C.free(unsafe.Pointer(cs))
+
+	if r := C.avformat_open_input(&formatCtx, cs, nil, nil); r < 0 {
+		return errors.New("Failed to open input file" + fileName)
+	}
 
 	frame = C.av_frame_alloc()
 	defer C.av_frame_free(&frame)
-
-	file, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return err
-	}
 
 	packet = (*C.AVPacket)(C.malloc(C.size_t(unsafe.Sizeof(C.AVPacket{}))))
 	defer C.free(unsafe.Pointer(packet))
 	C.av_init_packet(packet)
 
-	packet.data = (*C.uint8_t)(&file[0])
-	packet.size = C.int(len(file))
+	C.av_read_frame(formatCtx, packet)
+	if C.avcodec_decode_video2(d.codecCtx, frame, &got, packet) < 0 {
+		return errors.New("Unable to decode")
+	}
+	if got == 0 {
+		return errors.New("Didn't get any data")
+	}
+	fmt.Println("Video width:", frame.width)
+	fmt.Println("Video height:", frame.height)
 
-	C.av_read_frame(s, packet)
-	//if C.avcodec_decode_video2(d.codecCtx, frame, &got, packet) < 0 {
-	//	return errors.New("Unable to decode")
-	//}
-	//if got == 0 {
-	//	return errors.New("Didn't get any data")
-	//}
+	if r := C.avpicture_alloc(pict, d.codecCtx.pix_fmt, frame.width, frame.height); r < 0 {
+		return errors.New("Failed to allocate picture buffer")
+	}
+	defer C.avpicture_free(pict)
+	if r := C.avpicture_fill(pict, frame.data, d.codecCtx.pix_fmt, frame.width, frame.height); r < 0 {
+		return errors.New("Failed to fill picture")
+	}
+
 	return nil
 }
