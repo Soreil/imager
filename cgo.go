637e4e28423600c06c14a92826258c3324a78912
Sjon <sjon@x61.eu>
2016-02-15 22:23:42 +0100

    More work on the ffmpeg transforms, removed deprecated function usage, stuck on reading in to AVPacket now

--- cgo.go
+++ cgo.go
@@ -21,46 +21,60 @@ func init() {
 }
 
 type Decoder struct {
-	codec    *C.AVCodec
-	codecCtx *C.AVCodecContext
+	codec     *C.AVCodec
+	codecCtx  *C.AVCodecContext
+	formatCtx *C.AVFormatContext
 }
 
 //VP8 only, need to figure the logic behind the header out, maybe this is a versioning thing?
 func NewVP8Decoder() *Decoder {
-	var dec Decoder
-	dec.codec = C.avcodec_find_decoder(C.AV_CODEC_ID_VP8)
-	if dec.codec == nil {
-		panic(errors.New("Can't find codec"))
-	}
-	dec.codecCtx = C.avcodec_alloc_context3(dec.codec)
-	if dec.codecCtx == nil {
-		panic(errors.New("Can't create context"))
-	}
-	dec.codecCtx.opaque = unsafe.Pointer(&dec)
-	C.avcodec_open2(dec.codecCtx, dec.codec, nil)
-	return &dec
+	//var dec Decoder
+	//dec.codec = C.avcodec_find_decoder(C.AV_CODEC_ID_VP8)
+	//if dec.codec == nil {
+	//	panic(errors.New("Can't find codec"))
+	//}
+	//dec.codecCtx = C.avcodec_alloc_context3(dec.codec)
+	//if dec.codecCtx == nil {
+	//	panic(errors.New("Can't create context"))
+	//}
+	//dec.codecCtx.opaque = unsafe.Pointer(&dec)
+	//C.avcodec_open2(dec.codecCtx, dec.codec, nil)
+	//return &dec
+	return &Decoder{}
 }
 
 func (d *Decoder) DecodeVP8(fileName string) error {
-	var packet *C.AVPacket
-	var frame *C.AVFrame
-	var formatCtx *C.AVFormatContext = nil
-	var got C.int
 
 	cs := C.CString(fileName)
 	defer C.free(unsafe.Pointer(cs))
 
-	if r := C.avformat_open_input(&formatCtx, cs, nil, nil); r < 0 {
+	if r := C.avformat_open_input(&d.formatCtx, cs, nil, nil); r < 0 {
 		return errors.New("Failed to open input file" + fileName)
 	}
-	if r := C.av_find_best_stream(formatCtx, C.AVMEDIA_TYPE_VIDEO, -1, -1, &d.codec, 0); r < 0 {
+	if r := C.avformat_find_stream_info(d.formatCtx, nil); r < 0 {
+		return errors.New("Stream info broken")
+	}
+	if r := C.av_find_best_stream(d.formatCtx, C.AVMEDIA_TYPE_VIDEO, -1, -1, &d.codec, 0); r < 0 {
 		return errors.New("Failed to locate best video codec")
 	}
 
-	//strm := C.av_find_best_stream(formatCtx, C.AVMEDIA_TYPE_VIDEO, -1, -1, &d.codec, 0)
-	fmt.Printf("%+v\n", d.codecCtx)
-	d.codecCtx = (**formatCtx.streams).codec
-	fmt.Printf("%+v\n", d.codecCtx)
+	//strm is an offset for the d.formatCtx array, assuming it is always 0
+	//in our use cases skipping it should be fine right?
+
+	strm := C.av_find_best_stream(d.formatCtx, C.AVMEDIA_TYPE_VIDEO, -1, -1, &d.codec, 0)
+	if strm != 0 {
+		return errors.New("Video stream not at position zero:" + fmt.Sprint(strm))
+	}
+
+	d.codecCtx = (**d.formatCtx.streams).codec
+	//fmt.Printf("%+v\n", d.codecCtx)
+
+	//THIS CONFLICTS WITH THE DECODER NEW FUNCTION
+	if r := C.avcodec_open2(d.codecCtx, d.codec, nil); r < 0 {
+		return errors.New("Failed to open codec")
+	}
+	// ... or does it? At least this here too "works"
+	//fmt.Printf("%+v\n", d.codecCtx)
 
 	//SWSCONTEXT
 	var swCtx *C.struct_SwsContext
@@ -73,25 +87,36 @@ func (d *Decoder) DecodeVP8(fileName string) error {
 		C.SWS_FAST_BILINEAR, nil, nil, nil)
 	//SWSCONTEXT
 
-	frame = C.av_frame_alloc()
+	//DECODING PART OF THE FUNCTION
+
+	//Allocate frame data receiver packet
+	var packet C.AVPacket
+
+	if r := C.av_read_frame(d.formatCtx, &packet); r < 0 {
+		return errors.New("Failed to initialize packet from formatContext")
+	}
+
+	//Dead pointer code
+	//packet = (*C.AVPacket)(C.malloc(C.size_t(unsafe.Sizeof(C.AVPacket{}))))
+	//defer C.free(unsafe.Pointer(packet))
+	//C.av_init_packet(packet)
+
+	//Allocate the frame itself
+	var frame *C.AVFrame = C.av_frame_alloc()
 	defer C.av_frame_free(&frame)
 
-	packet = (*C.AVPacket)(C.malloc(C.size_t(unsafe.Sizeof(C.AVPacket{}))))
-	defer C.free(unsafe.Pointer(packet))
-	C.av_init_packet(packet)
-
-	C.av_read_frame(formatCtx, packet)
-	if C.avcodec_decode_video2(d.codecCtx, frame, &got, packet) < 0 {
+	var got C.int
+	if C.avcodec_decode_video2(d.codecCtx, frame, &got, &packet) < 0 {
 		return errors.New("Unable to decode")
+	} else if got == 0 {
+		return errors.New("Didn't get any data from decoding video")
 	}
-	if got == 0 {
-		return errors.New("Didn't get any data")
-	}
+
 	fmt.Println("Video width:", frame.width)
 	fmt.Println("Video height:", frame.height)
 
 	//Allocate output frame
-	var pngFrame *C.AVFrame = C.avcodec_alloc_frame()
+	var pngFrame *C.AVFrame = C.av_frame_alloc()
 	//Allocate picture sub thing of output frame
 	C.avpicture_alloc((*C.AVPicture)(unsafe.Pointer(&pngFrame)), C.PIX_FMT_RGB24, d.codecCtx.width, d.codecCtx.height)
 	//sws_scale in to the new frame
--- cgo_test.go
+++ cgo_test.go
@@ -2,9 +2,16 @@ package imager
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
 func TestDecodeVP8(t *testing.T) {
-	fmt.Println(NewVP8Decoder().DecodeVP8("test.webm"))
+	var fileName = "test.webm"
+	if _, err := os.Stat(fileName); err != nil {
+		t.Fatal(err)
+	}
+	if err := NewVP8Decoder().DecodeVP8(fileName); err != nil {
+		t.Fatal("Failed to thumbnail " + fileName + ": " + fmt.Sprint(err))
+	}
 }
