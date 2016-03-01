cb13c2cd09b1e0e162d2a83962de8d4dfd3cd077
Sjon <sjon@x61.eu>
2016-03-01 15:39:37 +0100

    Initial tests for webms

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # imager
-CGo FFMPEG bindings for thumbnailing.
+CGo FFmpeg bindings for thumbnailing WebM video.
+CGo PNGQuant bindings for reducing PNG file sizes.
+
+The goal is to have GIF,PNG,WEBM,JPEG all be represented by a single intermediary pixel format and raw pixel data.
+This will be worked on by the Go image package, the idea for now it to have it all be RGBA.
+
+This will be scaled to 250x250 for low quality thumb and 500x500 for high quality.
+
+Only JPEG has no transparency, for that the output format is JPEG too because of that
+The rest all need PNG thumbnails to maintain transparency.
+Well GIF could have GIF thumbnails.
+
--- cgo_test.go
+++ cgo_test.go
@@ -1,14 +1,157 @@
 package imager
 
 import (
+	"image"
+	_ "image/gif"
+	"image/jpeg"
+	"image/png"
 	"os"
 	"testing"
+
+	_ "github.com/Soreil/imager/webm"
 )
 
-func TestDecodeVP8(t *testing.T) {
-	var fileName = "test.webm"
-	if _, err := os.Stat(fileName); err != nil {
+const dataDirectory = "testData/"
+
+//func TestWebmToJPG(t *testing.T) {
+//	const filename = dataDirectory + "wafel.webm"
+//	if _, err := os.Stat(filename); err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	//TODO(sjon):add this stuff in to a pipeline
+//	frame := extractVideoFrame(filename)
+//	img := avFrameImage(&frame)
+//
+//	out, err := os.Create(filename + ".jpg")
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	i := scale(&img, normal)
+//	if err := jpeg.Encode(out, i, nil); err != nil {
+//		t.Fatal(err)
+//	}
+//}
+
+func TestWebmToPNG(t *testing.T) {
+	const filename = dataDirectory + "wafel.webm"
+	if _, err := os.Stat(filename); err != nil {
 		t.Fatal(err)
 	}
-	thumbnailWebm(fileName, "out.png")
+
+	//TODO(sjon):add this stuff in to a pipeline
+	frame := extractVideoFrame(filename)
+	img := avFrameImage(&frame)
+
+	out, err := os.Create(filename + ".png")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	i := scale(&img, normal)
+	if err := png.Encode(out, i); err != nil {
+		t.Fatal(err)
+	}
 }
+
+func TestJPGToJPG(t *testing.T) {
+	const filename = dataDirectory + "yuno.jpg"
+	if _, err := os.Stat(filename); err != nil {
+		t.Fatal(err)
+	}
+
+	//TODO(sjon):add this stuff in to a pipeline
+	file, err := os.Open(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, _, err := image.Decode(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out, err := os.Create(filename + ".jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	img = scale(img, normal)
+	if err := jpeg.Encode(out, img, nil); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPNGToPNG(t *testing.T) {
+	const filename = dataDirectory + "yuno.png"
+	if _, err := os.Stat(filename); err != nil {
+		t.Fatal(err)
+	}
+
+	//TODO(sjon):add this stuff in to a pipeline
+	file, err := os.Open(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, _, err := image.Decode(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out, err := os.Create(filename + ".png")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	img = scale(img, normal)
+	if err := png.Encode(out, img); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGIFToPNG(t *testing.T) {
+	const filename = dataDirectory + "yuno.gif"
+	if _, err := os.Stat(filename); err != nil {
+		t.Fatal(err)
+	}
+
+	//TODO(sjon):add this stuff in to a pipeline
+	file, err := os.Open(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, _, err := image.Decode(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out, err := os.Create(filename + ".png")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	img = scale(img, normal)
+	if err := png.Encode(out, img); err != nil {
+		t.Fatal(err)
+	}
+
+}
+
+//TODO(sjon):Debug webm driver
+//func TestWebmPackage(t *testing.T) {
+//	const filename = dataDirectory + "wafel.webm"
+//	if _, err := os.Stat(filename); err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	//TODO(sjon):add this stuff in to a pipeline
+//	file, err := os.Open(filename)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	img, _, err := image.Decode(file)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	_ = img
+//}
